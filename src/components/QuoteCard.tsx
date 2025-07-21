6617e69562f00b3ec292796d115e3ca52a4849e2
Jose Fernando Guerra Muñoz <fernandoguerra344@gmail.com>
2025-07-21 15:20:23 -0600

    Delete send messages

--- src/components/QuoteCard.tsx
+++ src/components/QuoteCard.tsx
@@ -82,26 +82,7 @@ export default function QuoteCard() {
                 </button>
             </div>
 
-            <div className="bg-black/60 p-8 rounded-xl shadow-2xl text-center w-full max-w-md mb-10">
-                <h1 className="text-3xl font-bold mb-6 text-white">Crea un mensaje ✨</h1>
-                <div className="relative h-11 w-full min-w-[200px]">
-                  <input
-                    className="h-[10vh] peer w-full rounded-md border border-blue-gray-200 bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
-                    placeholder=" "
-                    value={message}
-                    onChange={handleOnChange}
-                  />
-                  <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-pink-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-pink-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-pink-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
-                    Escribe tu mensaje...
-                  </label>
-                </div>
-                <button
-                  onClick={sendMessage}
-                  className="mt-[6vh] h-[10vh] w-full rounded-lg bg-sky-500 py-3 px-6 text-xs font-bold uppercase text-white shadow-md hover:shadow-lg transition-all"
-                >
-                  Send
-                </button>
-            </div>
+            
 
             <footer className="w-full max-w-md p-4 bg-white rounded-lg shadow text-center">
                 <hr className="my-4 border-gray-300" />
